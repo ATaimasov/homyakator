52cc9b94d6860f661b5e0ade74953fefe69e006f
Alexander Taimasov <alexander.taimasov@gmail.com>
2024-06-16 01:08:48 +0300

    some features, some fixes...

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,45 @@
-const link1 = document.getElementsByClassName('link1');
-for (let i=0; i < link1.length; i++) {
-    link1[i].onclick = function() {
+const confirmTrue = document.querySelector('.confirm-true');
+const confirmFalse = document.querySelector('.confirm-false');
+const confirm = document.querySelector('.hamster-confirm');
+const backButton = document.querySelector('.back-button-false-confirm');
+const confirmFalseWindow = document.querySelector('.confirm-false-window');
+const preload = document.querySelector('.preload');
+document.body.style.overflow = 'hidden';
+
+confirmFalse.addEventListener ('click', getFalseWindow);
+
+function getFalseWindow() {
+        confirm.classList.add('hidden');
+        confirmFalseWindow.classList.remove('hidden');
+}
+
+backButton.addEventListener('click', () => {
+    confirm.classList.remove('hidden');
+    confirmFalseWindow.classList.add('hidden');
+})
+
+confirmTrue.addEventListener('click', () => {
+    preload.hidden = true;
+    document.body.style.overflow = '';
+})
+
+  
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('link1')) {
         document.getElementById("pick_your_korm").scrollIntoView({behavior: "smooth"});
-    }
-}
-
-const link2 = document.getElementsByClassName('link2');
-for (let i=0; i < link2.length; i++) {
-    link2[i].onclick = function() {
+    } else if (event.target.classList.contains('link2')) {
         document.getElementById("about-us1").scrollIntoView({behavior: "smooth"});
-    }
-}
-
-const link3 = document.getElementsByClassName('link3');
-for (let i = 0; i < link3.length; i++) {
-    link3[i].onclick = function() {
+    } else if (event.target.classList.contains('link3')) {
         document.getElementById("reviews").scrollIntoView({behavior: "smooth"});
-    }
-}
-
-const link4 = document.getElementsByClassName('link4');
-for (let i= 0; i < link4.length; i++) {
-    link4[i].onclick = function() {
+    } else if (event.target.classList.contains('link4')) {
         document.getElementById('contacts').scrollIntoView({behavior: "smooth"});
     }
-}
+});
+
+
+
+
 
 function playAudio() {
     const homyakator = document.getElementById('homyakator');
@@ -32,3 +47,7 @@ function playAudio() {
     homyakator.volume = 0.2;
 
 }
+
+
+
+
